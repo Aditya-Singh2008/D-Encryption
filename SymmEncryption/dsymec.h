6337c3bc60081aa3817e93e097ea256b54d746c9
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2022-08-17 10:16:25 +0000

    Update dsymec.h
    write the input straight to the block

--- SymmEncryption/dsymec.h
+++ SymmEncryption/dsymec.h
@@ -8,6 +8,7 @@ void xor2(int *pta);
 void textout(int *pta, int total);
 void ippass(int *pointer_ascii);
 int blockphrase(int *pointer_ascii, BLOCK32 *pointer_block, int block_no);
+int blockwrite(BLOCK32 *pointer_block, int block_count);
 
 typedef struct BLOCK32
 {
@@ -21,13 +22,38 @@ int charlen;
 int *tempascii = NULL;
 int *ptrascii = NULL;
 
+int blockwrite(BLOCK32 *pointer_block, int block_count)
+{
+    int block_element = 0, input;
+	while(block_element < block_count)
+	{
+		for(int yaxis = 0; yaxis < 8; yaxis++)
+		{
+			for(int xaxis = 0; xaxis < 4; xaxis++)
+			{
+				if((input = getchar()) != EOF)
+				{
+					(pointer_block + block_element)->bl[yaxis][xaxis] = input;
+				}
+				else
+				{
+					(pointer_block + block_element)->bl[yaxis][xaxis] = 0;
+					goto breakout;
+				}
+			}
+		}
+        block_element++;
+	}
+	breakout:
+	return (block_element + 1);
+}
+
 void ippass(int *pointer_ascii)
 {
 	while((*pointer_ascii++ = getchar()) != EOF)
 		;
 	*--pointer_ascii = '\0';
 }
-
 
 int blockphrase(int *pointer_ascii, BLOCK32 *pointer_block, int block_no)
 {
@@ -55,63 +81,74 @@ int blockphrase(int *pointer_ascii, BLOCK32 *pointer_block, int block_no)
 	return (block_element + 1);
 }
 
-int inputfilelen(char *filesname){
-  FILE *readfile = fopen(filesname, "r");
-  int input;
-  int sum = 1;
+int inputfilelen(char *filesname)
+{
+	FILE *readfile = fopen(filesname, "r");
+	int input;
+	int sum = 1;
 
-  if(readfile == NULL){
-    printf("Enter a valid file name");
-    exit(1);
-  }
+	if(readfile == NULL)
+	{
+    		printf("Enter a valid file name");
+    		exit(1);
+  	}
 
-  while((*ptrascii++ = (input = fgetc(readfile))) != EOF){
-    sum++;
-  }
-  fclose(readfile);
-  ptrascii -= sum;
-  return sum;
+  	while((*ptrascii++ = (input = fgetc(readfile))) != EOF)
+	{
+    		sum++;
+  	}
+  	fclose(readfile);
+  	ptrascii -= sum;
+  	return sum;
 }
 
-int filewrite(char *filesname, int *pta, int charlen){
-  FILE *writefile = fopen(filesname, "w");
-  int element = 0;
-  while(*(pta + element) != EOF){
-    fputc(*(pta + element), writefile);
-    element++;
-  }
-  fclose(writefile);
+int filewrite(char *filesname, int *pta, int charlen)
+{
+  	FILE *writefile = fopen(filesname, "w");
+  	int element = 0;
+  	while(*(pta + element) != EOF)
+	{
+    		fputc(*(pta + element), writefile);
+    		element++;
+  	}
+  	fclose(writefile);
 }
 
-int processinput(){
-  printf("[Enter Input]: ");
-  int input;
-  int sum = 1;
-  while((*ptrascii++ = (input = getchar())) != -1){
-    sum++;
-  }
-  ptrascii -= sum;
-  return sum;
+int processinput()
+{
+  	printf("[Enter Input]: ");
+  	int input;
+  	int sum = 1;
+  	while((*ptrascii++ = (input = getchar())) != -1)
+	{
+    		sum++;
+  	}
+	ptrascii -= sum;
+  	return sum;
 }
 
-void getkey(){
-  printf("[Enter Key]: ");
-  int keyinput;
-  int element = 0;
-  int expendno = 0;
+void getkey()
+{
+	printf("[Enter Key]: ");
+  	int keyinput;
+  	int element = 0;
+  	int expendno = 0;
 
-  while((keyinput = getchar()) != '\n'){
-    key[element] = keyinput;
-    keyxored[element] = key[element] ^ keyexpand[expendno];
-    expendno++;
-    if(expendno == 4){
-      expendno = 0;
-    }
-    element++;
-  }
+  	while((keyinput = getchar()) != '\n')
+	{
+    		key[element] = keyinput;
+   		keyxored[element] = key[element] ^ keyexpand[expendno];
+    		expendno++;
+    		if(expendno == 4)
+		{
+      			expendno = 0;
+    		}
+    		element++;
+  	}
 }
 
-void shiftrow1(int *pta){
+void shiftrow1(int *pta)
+{
   int swicher;
   int *sw1 = pta;
   int *sw2 = (pta + 1);
@@ -120,14 +157,16 @@ void shiftrow1(int *pta){
   *sw1 = *sw2;
   *sw2 = swicher;
 
-  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1){
+  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1)
+  {
     swicher = *sw1;
     *sw1 = *sw2;
     *sw2 = swicher;
   }
 }
 
-void shiftrow2(int *pta){
+void shiftrow2(int *pta)
+{
   int swicher;
   int *sw1 = (pta + 1);
   int *sw2 = (pta + 2);
@@ -136,7 +175,8 @@ void shiftrow2(int *pta){
   *sw1 = *sw2;
   *sw2 = swicher;
 
-  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1){
+  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1)
+  {
     swicher = *sw1;
     *sw1 = *sw2;
     *sw2 = swicher;
@@ -144,33 +184,41 @@ void shiftrow2(int *pta){
 }
 
 
-void xor1(int *pta){
+void xor1(int *pta)
+{
   int *xr = pta;
   int keyelement = 0;
 
-  while(*xr != -1){
-    if(((*xr ^ key[keyelement]) > 31 || (*xr ^ key[keyelement]) == 10)){
+  while(*xr != -1)
+  {
+    if(((*xr ^ key[keyelement]) > 31 || (*xr ^ key[keyelement]) == 10))
+    {
       *xr = *xr ^ key[keyelement];
     }
     keyelement++;
     xr++;
-    if(keyelement == 3){
+    if(keyelement == 3)
+    {
         keyelement = 0;
     }
   }
 }
 
-void xor2(int *pta){
+void xor2(int *pta)
+{
   int *xr = pta;
   int keyelement = 0;
 
-  while(*xr != -1){
-    if((*xr ^ keyxored[keyelement]) > 31 || (*xr ^ keyxored[keyelement]) == 10){
+  while(*xr != -1)
+  {
+    if((*xr ^ keyxored[keyelement]) > 31 || (*xr ^ keyxored[keyelement]) == 10)
+    {
       *xr = *xr ^ keyxored[keyelement];
     }
     keyelement++;
     xr++;
-    if(keyelement == 3){
+    if(keyelement == 3)
+    {
         keyelement = 0;
     }
   }
